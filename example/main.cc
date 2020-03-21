a21dd72c1d0d4dfba5ce717093ae550cf576be11
Felix Gündling <felix.guendling@gmail.com>
2020-03-21 18:42:32 +0100

    fix example, improve example (read file from cmd line, continue on error)

--- example/main.cc
+++ example/main.cc
@@ -1,6 +1,9 @@
-#include <iostream>
-#include <fstream>
 #include <chrono>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <utility>
+#include <vector>
 
 #include "guess/guesser.h"
 
@@ -15,35 +18,46 @@ struct new_line_tokenizer : std::ctype<char> {
   }
 };
 
-int main() {
-  std::vector<std::string> stations;
+int main(int argc, char const** argv) {
+  if (argc != 2) {
+    std::cout << "Usage: " << argv[0] << " FILENAME\n";
+    return 1;
+  }
 
+  auto line = 0U;
+  std::vector<std::pair<std::string, float>> stations;
   try {
-    std::fstream in("stations.txt");
+    std::fstream in(argv[1]);
     in.exceptions(std::ifstream::failbit);
     in.imbue(std::locale(std::cin.getloc(), new new_line_tokenizer));
-    std::copy(std::istream_iterator<std::string>(in),
-              std::istream_iterator<std::string>(),
-              std::back_inserter(stations));
+    for (auto it = std::istream_iterator<std::string>(in),
+              end = std::istream_iterator<std::string>();
+         it != end; ++it) {
+      stations.emplace_back(*it, 1.0);
+      ++line;
+    }
   } catch (std::exception const& e) {
-    std::cout << "unable to read file stations.txt\n";
-    return 1;
+    std::cout << "WARNING file read error at line " << line << " : " << e.what()
+              << "\n ";
+    if (stations.empty()) {
+      return 1;
+    }
   }
 
   guesser g(stations);
 
   std::string input;
   while (std::cout << "$ " && std::getline(std::cin, input)) {
-    using std::chrono::system_clock;
     using std::chrono::duration_cast;
     using std::chrono::milliseconds;
+    using std::chrono::system_clock;
     auto start = system_clock::now();
-    auto candidates = g.guess(input);
+    auto candidates = g.guess_match(input);
     auto duration = duration_cast<milliseconds>(system_clock::now() - start);
     std::cout << "\n\t" << duration.count() << "ms\n\n";
 
     for (auto const& guess : candidates) {
-      std::cout << "  " << stations[guess] << "\n";
+      std::cout << "  " << stations[guess.index].first << "\n";
     }
     std::cout << "\n";
   }
